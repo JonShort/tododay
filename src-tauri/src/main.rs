1a4678cf359473558ec9c6f885063a7fce4c011f
JonShort <JonShort@users.noreply.github.com>
2023-03-01 20:37:29 +0000

    [backend] - stub out sync client method

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,24 +3,20 @@
     windows_subsystem = "windows"
 )]
 
-#[derive(Clone, serde::Serialize)]
-struct RefreshPayload {}
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn add_todo(todo: &str) -> String {
-    format!("Added your todo: {}", todo)
-}
-
 #[tauri::command]
 fn get_todos() -> String {
     // get todos from file
     String::from("{\"aaaaa\":{\"addedAt\":1677528896913,\"content\":\"Do thing\",\"isComplete\":false},\"bbbbb\":{\"addedAt\":1677528898225,\"content\":\"Do some things\",\"isComplete\":false}}")
 }
 
+#[tauri::command]
+fn sync() -> bool {
+    true
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![add_todo, get_todos])
+        .invoke_handler(tauri::generate_handler![get_todos, sync])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
