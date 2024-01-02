bc0836ce496d71e755b2354c907d23f628717c4c
Jon Short <JonShort@users.noreply.github.com>
2024-01-02 16:23:52 +0000

    Update dependencies and avoid GHSA-2rcp-jvr4-r259 (#18)
    * [client|deps] - update tauri
    
    * [backend|deps] - tauri bump
    
    * [config] - config adjust to address GHSA-2rcp-jvr4-r259
    
    * [client|deps] - non-major bumps
    
    * [client|deps] - nanoid v5
    
    * [client|deps] - vite v5

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,16 @@ export default defineConfig({
     port: 1420,
     strictPort: true,
   },
-  // to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+  // to access the Tauri environment variables set by the CLI with information about the current target
+  envPrefix: [
+    "VITE_",
+    "TAURI_PLATFORM",
+    "TAURI_ARCH",
+    "TAURI_FAMILY",
+    "TAURI_PLATFORM_VERSION",
+    "TAURI_PLATFORM_TYPE",
+    "TAURI_DEBUG",
+  ],
   build: {
     // Tauri supports es2021
     target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
